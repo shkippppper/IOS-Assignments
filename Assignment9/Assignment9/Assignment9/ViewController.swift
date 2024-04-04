ff5962fb0a1d12021fef430354637f373fa3eddb
Akaki <a.titberidze@yahoo.com>
2024-04-04 22:00:02 +0400

    Add optional after deadline

--- Assignment9/Assignment9/Assignment9/ViewController.swift
+++ Assignment9/Assignment9/Assignment9/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //უმცირესი საერთო ჯერადი LCM
     //უდიდესი საერთო გამყოფი GCD
     
@@ -16,38 +16,68 @@ class ViewController: UIViewController {
     var number2: Int = 0
     var isGCD: Bool = false
     
+    @IBOutlet weak var calculateButtonUI: UIButton!
+    @IBOutlet weak var buttonOutlet: UIButton!
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var switchController: UISwitch!
-
+    
     
     @IBAction func switchMode(_ sender: UISwitch) {
         isGCD = !isGCD
         if isGCD {
+            calculateButtonUI.backgroundColor = UIColor.systemBlue
+            calculateButtonUI.layer.borderColor = UIColor.systemBlue.cgColor
             mainLabel.text = "უდიდესი საერთო გამყოფი"
         }else{
+            calculateButtonUI.backgroundColor = UIColor.systemGreen
+            calculateButtonUI.layer.borderColor = UIColor.systemGreen.cgColor
             mainLabel.text = "უმცირესი საერთო ჯერადი"
         }
     }
     
     @IBAction func num1Input(_ sender: UITextField) {
-        if let input = sender.text{
+        if let input = sender.text, Int(input) != nil {
             number1 = Int(input) ?? 0
+            sender.layer.borderWidth = 0
+            buttonOutlet.isEnabled = true
+            buttonOutlet.alpha = 1
+        } else {
+            sender.layer.borderWidth = 1
+            sender.layer.borderColor = UIColor.red.cgColor
+            buttonOutlet.isEnabled = false
+            buttonOutlet.alpha = 0.5
         }
     }
     
     @IBAction func num2Input(_ sender: UITextField) {
-        if let input = sender.text{
+        if let input = sender.text, Int(input) != nil {
             number2 = Int(input) ?? 0
+            sender.layer.borderWidth = 0
+            buttonOutlet.isEnabled = true
+            buttonOutlet.alpha = 1
+        } else {
+            sender.layer.borderWidth = 1
+            sender.layer.borderColor = UIColor.red.cgColor
+            buttonOutlet.isEnabled = false
+            buttonOutlet.alpha = 0.5
         }
     }
     
     @IBAction func calcButton(_ sender: UIButton) {
         var result: Int
         if isGCD {
+            buttonOutlet.isEnabled = false
+            buttonOutlet.alpha = 0.5
             result = calculateGCD(num1: number1, num2: number2)
+            buttonOutlet.isEnabled = true
+            buttonOutlet.alpha = 1
         } else {
+            buttonOutlet.isEnabled = false
+            buttonOutlet.alpha = 0.5
             result = calculateLCM(num1: number1, num2: number2)
+            buttonOutlet.isEnabled = true
+            buttonOutlet.alpha = 1
         }
         
         answerLabel.text = "პასუხი: \(result)"
@@ -57,8 +87,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         mainLabel.text = "უმცირესი საერთო ჯერადი"
         answerLabel.text = "პასუხი:"
+        calculateButtonUI.layer.cornerRadius = 5
+        calculateButtonUI.backgroundColor = UIColor.systemGreen
+        calculateButtonUI.layer.borderColor = UIColor.systemGreen.cgColor
     }
-
+    
     
     func calculateLCM(num1: Int, num2: Int) -> Int{
         var greater = max(num1, num2)
